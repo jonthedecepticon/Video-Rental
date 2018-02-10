9e28627a42531491f053a28c3bee0dd5e9eab294
Jon Lambson <jonlambson@gmail.com>
2018-02-09 23:42:43 -0700

    added new css

--- vid/Models/Movie.cs
+++ vid/Models/Movie.cs
@@ -3,8 +3,9 @@ namespace vid.Models
 {
     public class Movie
     {
-        public Movie()
-        {
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
+
+
 }
